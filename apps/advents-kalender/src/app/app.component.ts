03551944e34ff509eee6387d5d1d92daf8e44587
Andreas Kuhlmann <Andreas.Kuhlmann@avareto.com>
2021-09-03 18:02:19 +0200

    changes

--- apps/advents-kalender/src/app/app.component.ts
+++ apps/advents-kalender/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 import { AdventDay, AdventDaysService } from './services/advent-days.service';
 
 @Component({
@@ -7,12 +13,24 @@ import { AdventDay, AdventDaysService } from './services/advent-days.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  @ViewChild('grid', { read: ElementRef }) grid: ElementRef | undefined;
   title = 'advents-kalender';
+  gridXPosition = 0;
+  gridYPosition = 0;
+  gridWidth = 1095;
+  gridHeight = 768;
   constructor(public days: AdventDaysService) {}
   flip(day: AdventDay) {
     this.days.flipDay(day);
   }
   async ngOnInit() {
     await this.days.loadDays();
+    if (this.grid) {
+      this.gridXPosition = this.grid.nativeElement.getBoundingClientRect().x;
+      this.gridYPosition = this.grid.nativeElement.getBoundingClientRect().y;
+      this.gridWidth = this.grid.nativeElement.getBoundingClientRect().width;
+      this.gridHeight = this.grid.nativeElement.getBoundingClientRect().height;
+      console.log(`${this.gridXPosition} ${this.gridYPosition} ${this.gridWidth} ${this.gridHeight}`);
+    }
   }
 }
--- apps/advents-kalender/src/app/door/door.component.ts
+++ apps/advents-kalender/src/app/door/door.component.ts
@@ -1,14 +1,55 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'door',
   templateUrl: './door.component.html',
   styleUrls: ['./door.component.scss'],
 })
-export class DoorComponent implements OnInit {
-  @Input() flipped: boolean = false;
+export class DoorComponent implements OnChanges, AfterViewInit {
+  @Input() baseX = 0;
+  @Input() baseY = 0;
+  @Input() baseWidth = 0;
+  @Input() baseHeight = 0;
+  @Input() no = 0;
+  @Input() isOpen = false;
+  @Input() cannotOpen = true;
+  @Input() text: string | undefined;
+  backgroundPosition = '';
+  @ViewChild('door', { read: ElementRef }) door: ElementRef | undefined;
 
-  constructor() {}
+  keepClose = false;
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    setTimeout(() => this.setBackgroundPosition());
+  }
+
+  ngOnChanges(): void {
+    this.setBackgroundPosition();
+  }
+  toggle(): void {
+    if (!this.isOpen) {
+      this.keepClose = true;
+      setTimeout(() => (this.keepClose = false), 1000);
+    }
+  }
+
+  private setBackgroundPosition() {
+    if (this.door) {
+      const posX = this.door.nativeElement.getBoundingClientRect().x;
+      const posY = this.door.nativeElement.getBoundingClientRect().y;
+      const backgroundPostionX =
+        Math.floor(this.baseX - posX).toString() + 'px';
+      const backgroundPostionY =
+        Math.floor(this.baseY - posY).toString() + 'px';
+      this.backgroundPosition = `${backgroundPostionX} ${backgroundPostionY}`;
+      console.log(this.backgroundPosition);
+    }
+  }
 }
