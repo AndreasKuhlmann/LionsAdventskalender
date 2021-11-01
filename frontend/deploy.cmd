cd8b1f2dd7f1d2abc9db4dc8be8941546b997a71
Andreas Kuhlmann <Andreas.Kuhlmann@avareto.com>
2021-11-01 21:00:04 +0100

    added new manifest

--- frontend/deploy.cmd
+++ frontend/deploy.cmd
@@ -7,7 +7,7 @@ azcopy sync dist\apps\adventskalender "https://kalendar.blob.core.windows.net/$w
 @REM azcopy remove "https://kalendar.blob.core.windows.net/$web??sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-10-31T22:43:50Z&st=2021-10-31T14:43:50Z&spr=https&sig=35kbPyWiSgLfB7YO%%2Fwt2DCJ6GaRWT2i9rOMBx3bcfFA%%3D" --recursive=true
 @REM azcopy copy "dist\apps\adventskalender\*" "https://kalendar.blob.core.windows.net/$web??sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-10-31T22:43:50Z&st=2021-10-31T14:43:50Z&spr=https&sig=35kbPyWiSgLfB7YO%%2Fwt2DCJ6GaRWT2i9rOMBx3bcfFA%%3D" --recursive=true --overwrite=true
 
-az cdn endpoint purge --resource-group adventskalendar --profile-name kalendar --name kalendar --content-paths=/*
+az cdn endpoint purge --resource-group adventskalender --profile-name kalendar --name kalendar --content-paths=/*
 
 @REM cmd.exe /c az storage blob delete-batch --source $web --account-name kalendar
 @REM cmd.exe /c az storage blob upload-batch --destination $web --source ./dist --account-name kalendar
